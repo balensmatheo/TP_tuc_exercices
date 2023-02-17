525f54fab88f5612f0bbf455eaebbf7a3daf9aaa
balensmatheo <matheobalens@gmail.com>
2023-02-17 15:46:01 +0100

    add iframe

--- main2.py
+++ main2.py
@@ -4,15 +4,22 @@ import time
 from selenium.webdriver.common.by import By
 
 # Path: selenium/webdriver.py
+driver = webdriver.Chrome() 
 
 def get_random_wiki():
-    driver = webdriver.Chrome() 
     driver.get('https://wikiroulette.co/')
-    page_title = driver.find_element(By.ID, 'firstHeading')
-    driver.quit()
-    return page_title
+    # get iframe with the id iframe
+    iframe = driver.find_elements(by=By.TAG_NAME, value='iframe')
+    src = iframe[0].get_attribute('src')
+    driver.get(src)
+    # get the title of the page
+    title = driver.find_elements(by=By.CLASS_NAME, value='mw-page-title-main')
+    return title[0].text
 
-get_random_wiki()
+
+print(get_random_wiki())
+
+driver.quit()
 
 
 
